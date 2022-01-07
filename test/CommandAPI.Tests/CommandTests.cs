54711451c715d06b7d494a2ce2cf5f7ad762b7f8
Carlos M <carlosmdelc@gmail.com>
2022-01-07 10:05:57 -0600

    Adding Tests

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do Something",
+                Platform = "Some platform",
+                CommandLine = "Some commandline"
+            };
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            // Arrange
+
+            // Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            // Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            // Arrange
+
+            // Act
+            testCommand.Platform = "xUnit";
+
+            // Assert
+            Assert.Equal("xUnit", testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            // Arrange
+
+            // Act
+            testCommand.CommandLine = "dotnet test";
+
+            // Assert
+            Assert.Equal("dotnet test", testCommand.CommandLine);
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+    }
+}
